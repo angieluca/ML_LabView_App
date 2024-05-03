bc4f101646d6a6a96e10c1b2616d50dc6cd006fe
eduluca <78570829+eduluca@users.noreply.github.com>
2024-05-03 12:37:05 -0400

    commit

--- LABVIEW ML LABELGEN/second version folder/build.py
+++ LABVIEW ML LABELGEN/second version folder/build.py
@@ -1,8 +1,18 @@
 import PyInstaller.__main__
+import os
+
+# Get the absolute path of the current script
+script_path = os.path.abspath(__file__)
+# Get the directory path of the script
+script_dir = os.path.dirname(script_path)
+# Construct the path to the model file relative to the script directory
+model_path = os.path.join(script_dir, 'best_pleth_ml_model_type2.pkl')
 
 PyInstaller.__main__.run([
-    '--name=FileExplorerApp',
+    '--name=LabelGenApp',
     '--onefile',
     '--windowed',
+    f'--add-data={model_path};.',
+    '--hidden-import=openpyxl',
     'labelgen_ml_edu.py'
 ])
--- LABVIEW ML LABELGEN/second version folder/labelgen_ml_edu.py
+++ LABVIEW ML LABELGEN/second version folder/labelgen_ml_edu.py
@@ -1,10 +1,17 @@
 import sys
+import os
 from PyQt5.QtWidgets import QApplication, QPushButton, QFileDialog, QVBoxLayout, QLabel, QWidget
 import numpy as np
 import pandas as pd
 import joblib
+import logging
 
-class FileExplorerApp(QWidget):
+def resource_path(relative_path):
+    if hasattr(sys, '_MEIPASS'):
+        return os.path.join(sys._MEIPASS, relative_path)
+    return os.path.join(os.path.abspath("."), relative_path)
+
+class LabelGenApp(QWidget):
     def __init__(self):
         super().__init__()
         self.initUI()
@@ -37,8 +44,10 @@ class FileExplorerApp(QWidget):
         fileName, _ = QFileDialog.getOpenFileName(self, "Open File", "", "All Files (*);;Python Files (*.py)", options=options)
 
         if fileName:
-            print("\nSelected file:", fileName, "\n")
+            #print("\nSelected file:", fileName, "\n")
+            logging.info("Starting file conversion..")
             converted_file = self.convert_clean(fileName)  # generate labels
+            logging.info("File conversion completed")
             self.label.setText("File should be opening :)")
             self.show()
 
@@ -51,72 +60,82 @@ class FileExplorerApp(QWidget):
                 subprocess.Popen(['xdg-open', converted_file])
 
     def convert_clean(self, new_path_type2):
-        # Read excel file
-        new_df_type2 = pd.read_excel(new_path_type2)
+        try:
+            # Read excel file
+            new_df_type2 = pd.read_excel(new_path_type2)
 
-        # Full column headers
-        full_column_headers = ['Sample', 'CPU Date', 'CPU Time', 'Site Time', 'Period Time', 'Protocol Type', 'Storage ID', 'First Beat ID', 'Last Beat ID', 'Ti (msec)', 'Te (msec)', 'PIF', 'PEF', 'TV', 'EV', 'RT (msec)', 'MV', 'P (msec)', 'f (bpm)', 'EIP (msec)', 'EEP (msec)', 'Penh', 'EF50', 'RH', 'Tbox', 'Tbody', 'Sr', 'Phase']
+            # Full column headers
+            full_column_headers = ['Sample', 'CPU Date', 'CPU Time', 'Site Time', 'Period Time', 'Protocol Type', 'Storage ID', 'First Beat ID', 'Last Beat ID', 'Ti (msec)', 'Te (msec)', 'PIF', 'PEF', 'TV', 'EV', 'RT (msec)', 'MV', 'P (msec)', 'f (bpm)', 'EIP (msec)', 'EEP (msec)', 'Penh', 'EF50', 'RH', 'Tbox', 'Tbody', 'Sr', 'Phase']
 
-        # Get ML column headers
-        column_headers = ['Ti (msec)', 'Te (msec)', 'PIF', 'PEF', 'TV', 'EV', 'RT (msec)', 'MV', 'P (msec)', 'f (bpm)', 'EIP (msec)', 'EEP (msec)', 'Penh', 'EF50', 'Sr', 'Phase']
+            # Get ML column headers
+            column_headers = ['Ti (msec)', 'Te (msec)', 'PIF', 'PEF', 'TV', 'EV', 'RT (msec)', 'MV', 'P (msec)', 'f (bpm)', 'EIP (msec)', 'EEP (msec)', 'Penh', 'EF50', 'Sr', 'Phase']
 
-        # Extract features
-        new_features_type2 = new_df_type2[column_headers]
+            # Extract features
+            new_features_type2 = new_df_type2[column_headers]
 
-        # Convert to numpy array
-        new_data_train_type2 = new_features_type2.to_numpy()
+            # Convert to numpy array
+            new_data_train_type2 = new_features_type2.to_numpy()
 
-        # Make predictions on the data
-        loaded_model_type2 = joblib.load('best_pleth_ml_model_type2.pkl')
-        pred_type2 = loaded_model_type2.predict(new_data_train_type2)
+            # Make predictions on the data
+            model_path = resource_path('best_pleth_ml_model_type2.pkl')
+            loaded_model_type2 = joblib.load(model_path)
+            pred_type2 = loaded_model_type2.predict(new_data_train_type2)
 
-        # Create a new column to store the generated labels
-        new_df_type2["Generated Labels"] = pred_type2
+            # Create a new column to store the generated labels
+            new_df_type2["Generated Labels"] = pred_type2
 
-        # Save predicted labels to 'generatelabeltest.npy' file
-        np.save('generatelabeltest.npy', pred_type2)
+            # Save predicted labels to 'generatelabeltest.npy' file
+            np.save('generatelabeltest.npy', pred_type2)
 
-        # Load the generated labels
-        generated_labels = np.load('generatelabeltest.npy')
+            # Load the generated labels
+            generated_labels = np.load('generatelabeltest.npy')
 
-        # Define label descriptions
-        label_descriptions = {
-            0: "Normal / Quiet Breath",
-            1: "Sigh breath",
-            2: "Sniffing breath",
-            3: "Random Apnea",
-            4: "Type II Apnea"
-        }
+            # Define label descriptions
+            label_descriptions = {
+                0: "Normal / Quiet Breath",
+                1: "Sigh breath",
+                2: "Sniffing breath",
+                3: "Random Apnea",
+                4: "Type II Apnea"
+            }
 
-        # Count the occurrences of each label
-        label_counts = np.unique(generated_labels, return_counts=True)
+            # Count the occurrences of each label
+            label_counts = np.unique(generated_labels, return_counts=True)
 
-        # Initialize total breath count
-        total_breath_count = 0
+            # Initialize total breath count
+            total_breath_count = 0
 
-        # Create a string to store the breath counts
-        count_text = ""
+            # Create a string to store the breath counts
+            count_text = ""
 
-        # Print the counts with descriptions
-        for label, count in zip(label_counts[0], label_counts[1]):
-            description = label_descriptions[int(label)]
-            count_text += f"{description} count ({int(label)}): {count}\n"
-            # Increment total breath count
-            total_breath_count += count
+            # Print the counts with descriptions
+            for label, count in zip(label_counts[0], label_counts[1]):
+                description = label_descriptions[int(label)]
+                count_text += f"{description} count ({int(label)}): {count}\n"
+                # Increment total breath count
+                total_breath_count += count
 
-        # Display the breath counts on the label
-        self.count_label.setText(count_text)
+            # Display the breath counts on the label
+            self.count_label.setText(count_text)
 
-        # Display the total breath count on the label
-        self.total_count_label.setText(f"Total breath count: {total_breath_count}")
+            # Display the total breath count on the label
+            self.total_count_label.setText(f"Total breath count: {total_breath_count}")
 
-        # Convert and export DataFrame to Excel with all columns
-        excel_file_path = new_path_type2[:-5] + "_LABELGEN.xlsx"  # change new file name
-        print("\nSuccess! New labeled file:", excel_file_path, "\n")
-        new_df_type2.to_excel(excel_file_path, index=False)
-        return excel_file_path
+            # Convert and export DataFrame to Excel with all columns
+            excel_file_path = new_path_type2[:-5] + "_LABELGEN.xlsx"  # change new file name
+            #print("\nSuccess! New labeled file:", excel_file_path, "\n")
+            new_df_type2.to_excel(excel_file_path, index=False)
+            return excel_file_path
+        
+        except FileNotFoundError as e:
+            logging.exception("File not found")
+
+        except Exception as e:
+            logging.exception("An error occured during file conversion:")
+
 
 if __name__ == '__main__':
+    logging.basicConfig(filename='labelgen.log', level=logging.ERROR, format='%(asctime)s - %(levelname)s - %(message)s')
     app = QApplication(sys.argv)
-    file_explorer = FileExplorerApp()
+    file_explorer = LabelGenApp()
     sys.exit(app.exec_())
